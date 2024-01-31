3eb11a32797fcfd56c10bf9870575226b1661025
YanaSurzh <158140839+YanaSurzh@users.noreply.github.com>
2024-01-31 10:44:01 +0300

    Add files via upload

--- testik/script.js
+++ testik/script.js
@@ -30,3 +30,36 @@ function formatDate(date) {
   let dateContainer = document.querySelector('.date-day');
   dateContainer.textContent = formattedDate;
   
+  function formatDate(date) {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+    const dayOfWeek = days[date.getDay()];
+    const dayOfMonth = date.getDate();
+    let daySuffix;
+  
+    // Add suffix to day of the month
+    if (dayOfMonth >= 11 && dayOfMonth <= 13) {
+      daySuffix = "th";
+    } else {
+      switch (dayOfMonth % 10) {
+        case 1:
+          daySuffix = "st";
+          break;
+        case 2:
+          daySuffix = "nd";
+          break;
+        case 3:
+          daySuffix = "rd";
+          break;
+        default:
+          daySuffix = "th";
+      }
+    }
+  
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+  
+    return `${dayOfWeek}, ${dayOfMonth}${daySuffix} ${month} ${year}`;
+  }
+    
